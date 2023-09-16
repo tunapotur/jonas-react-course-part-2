8e9c1c4188df546c797fbbe501576da3a4fb481d
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-09-16 11:48:23 +0300

    before section 12 fetch and effect

--- s11-eat-n-split-bug-fix/src/index.js
+++ s11-eat-n-split-bug-fix/src/index.js
@@ -1,23 +1,11 @@
 import React from "react";
-
 import ReactDOM from "react-dom/client";
-// import ReactDOM from 'react-dom';
-
-import App from "./App";
 import "./index.css";
+import App from "./App";
 
-// REACT 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// REACT 17
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
